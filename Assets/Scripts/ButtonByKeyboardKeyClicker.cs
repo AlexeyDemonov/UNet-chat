d351b2ba0c14ab03e18b5a830b81ffbfb87ec250
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2020-01-17 17:32:50 +0500

    ButtonByKeyboardKeyClicker added Added: ButtonByKeyboardKeyClicker script - gets keyboard key press at Update and invokes button's onClick event

---
+++ Assets/Scripts/ButtonByKeyboardKeyClicker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonByKeyboardKeyClicker : MonoBehaviour
+{
+    public KeyCode KeyCode;
+
+    Button _button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode))
+        {
+            _button.onClick.Invoke();
+        }
+    }
+}
